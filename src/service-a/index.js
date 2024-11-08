8f243ad7cfeed475326aba6ca94a404d82759988
Reid <reidandkat@gmail.com>
2024-11-07 18:33:45 -0600

    multi dont work

--- src/service-a/index.js
+++ src/service-a/index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const environment = process.env.ENVIRONMENT || "";
+const service = process.env.SERVICE || "";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = `Hello Node!\nCurrent environment: ${environment}\nService A`
+  const msg = `Hello Node!\nCurrent environment: ${environment}\nService ${service}\n`
   res.end(msg);
 });
 
